ddd95e3c82e2f317690dabf18d4dedda9a0c597a
Parham Javadi <pjfooeve09@yahoo.com>
2020-12-11 13:32:22 -0800

    commit tree 44d81c7f92716f85cc81389bd2ecf4bc2e7f5219

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
+#see what happens if we switch i+= 1 to the top
 def my_find(collection)
+    i = 0
+    while i < collection.length
+      return collection[i] if yield(collection[i])
+      i = i + 1
+    end
+  end 
 
-end
+  
